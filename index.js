3d6c1c96db2f2915876a4fa0e71fce0210e46ba3
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-06-15 14:26:04 -0500

    Inital commit

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+exports.RUNTIME_ARCHIVE_FILE = path.join(__dirname, 'libhlvm-runtime.a');
+exports.RUNTIME_DEFINITION_FILE = path.join(__dirname, 'lib.runtime.d.ts');
+//# sourceMappingURL=index.js.map
---
+++ math.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+
+#include "math.h"
+#include "machine.h"
+
+LIBRARY_EXPORT double Math__abs(double number) {
+    return std::abs(number);
+}
+
+LIBRARY_EXPORT double Math__pow(double number, double power) {
+    return std::pow(number, power);
+}
+
+LIBRARY_EXPORT double Math__sqrt(double number) {
+    return std::sqrt(number);
+}
+
+LIBRARY_EXPORT double Math__floor(double number) {
+    return std::floor(number);
+}
+
+LIBRARY_EXPORT double Math__round(double number) {
+    return std::round(number);
+}
+
+LIBRARY_EXPORT double Math__random() {
+    return Machine::Instance().getRandomNumberGenerator()->NextDouble();
+}
+
+struct TypedDoubleArray {
+    void *start;
+    int32_t size;
+};
+
+LIBRARY_EXPORT void* Array__constructor(double size) {
+    auto *ptr = new TypedDoubleArray();
+    ptr->size = (int32_t)size;
+    ptr->start = new double[(int32_t)size];
+
+    return ptr;
+}
+
+LIBRARY_EXPORT double Array__push(void *ptr, double number) {
+    auto *left = static_cast<TypedDoubleArray*>(ptr);
+
+    left->size++;
+    realloc(left, left->size * sizeof(double));
+
+    return left->size;
+}
